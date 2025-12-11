87415df7529ea7b4fa01cea751dd8a34cbcae443
AnthonyPortillaCano <anthony-nek29@outlook.com@github.com>
2025-12-11 02:23:51 -0500

    Avance semana 13

--- src/app.py
+++ src/app.py
@@ -12,6 +12,7 @@ Este app te permite generar queries MongoDB desde lenguaje natural.
 #API_URL = "http://localhost:8000/assist/"
 API_URL = "https://apppythonnek-graufac6ducma6ee.eastus-01.azurewebsites.net/assist/"
 
+
 # Entrada del usuario solo para la instrucción
 natural_text = st.text_area("Instrucción en lenguaje natural", value="crear campo user")
 
