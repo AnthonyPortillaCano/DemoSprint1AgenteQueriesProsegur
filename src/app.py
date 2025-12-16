53c9c8cd2b822e3f3c016eae68643092b7e5bce3
AnthonyPortillaCano <anthony-nek29@outlook.com@github.com>
2025-12-15 21:23:16 -0500

    Elimine archivo de workflow

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ Este app te permite generar queries MongoDB desde lenguaje natural.
 
 
 #API_URL = "http://localhost:8000/assist/"
-API_URL = "https://apppythonnek-graufac6ducma6ee.eastus-01.azurewebsites.net/assist/"
+API_URL = "https://prosegur-backend-app2025-hqe5f4dgb0fsfefs.eastus-01.azurewebsites.net/assist/"
 
 
 # Entrada del usuario solo para la instrucción
